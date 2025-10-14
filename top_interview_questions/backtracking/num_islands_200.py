1c96e33741e795db93e01b3b276161ea170f97e4
babydragon2 <thefoolsequence9@gmail.com>
2025-10-14 18:16:03 -0400

    whitespace

--- top_interview_questions/backtracking/num_islands_200.py
+++ top_interview_questions/backtracking/num_islands_200.py
@@ -1,5 +1,4 @@
 from typing import List
-
 # Backtracking DFS - O(mn) time w/ recursive stack space O(mn)
 
 
@@ -10,19 +9,19 @@ class Solution:
 
         def helper(i, j):
             grid[i][j] = "#"
-            if i+1 < rows and grid[i+1][j] == '1':
-                helper(i+1, j)
-            if i-1 >= 0 and grid[i-1][j] == '1':
-                helper(i-1, j)
-            if j-1 >= 0 and grid[i][j-1] == '1':
-                helper(i, j-1)
-            if j+1 < cols and grid[i][j+1] == '1':
-                helper(i, j+1)
+            if i + 1 < rows and grid[i + 1][j] == "1":
+                helper(i + 1, j)
+            if i - 1 >= 0 and grid[i - 1][j] == "1":
+                helper(i - 1, j)
+            if j - 1 >= 0 and grid[i][j - 1] == "1":
+                helper(i, j - 1)
+            if j + 1 < cols and grid[i][j + 1] == "1":
+                helper(i, j + 1)
 
         count = 0
         for r in range(rows):
             for c in range(cols):
-                if grid[r][c] == '1':
+                if grid[r][c] == "1":
                     count += 1
                     helper(r, c)
 
